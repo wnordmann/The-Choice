b15b8a93e25624b32266eb0140e0b4241b2797a6
william nordmann <william.nordmann@gmail.com>
2025-11-11 13:06:59 -0600

    Adding a deploy for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line:
+  base: '/The-Choice/', 
 })
